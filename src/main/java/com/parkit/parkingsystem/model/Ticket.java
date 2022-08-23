90bf251cd1330befb66da2adbcceb0ca09d032a0
Fercak Florian <fercak.florian@hotmail.fr>
2022-08-23 18:03:17 +0200

    Interface Cloneable implementee par Ticket et reecriture de la methode clone

--- src/main/java/com/parkit/parkingsystem/model/Ticket.java
+++ src/main/java/com/parkit/parkingsystem/model/Ticket.java
@@ -2,13 +2,24 @@ package com.parkit.parkingsystem.model;
 
 import java.util.Date;
 
-public class Ticket {
+public class Ticket implements Cloneable {
     private int id;
     private ParkingSpot parkingSpot;
     private String vehicleRegNumber;
     private double price;
     private Date inTime;
     private Date outTime;
+
+    @Override
+    public Ticket clone() {
+	Ticket ticket = null;
+	try {
+	    ticket = (Ticket) super.clone();
+	} catch (CloneNotSupportedException cnse) {
+	    cnse.printStackTrace(System.err);
+	}
+	return ticket;
+    }
 
     public int getId() {
 	return id;
